abec178eb22e5f7b7d01bf9057ae226d198469b2
Stephanie Caress <stephaniecaress@Stephanies-MacBook-Pro.local>
2020-06-02 23:08:43 -0700

    New app flow

--- app.py
+++ app.py
@@ -3,128 +3,13 @@ import numpy as np
 import requests
 from bs4 import BeautifulSoup
 import pickle
+import musicbrainzngs
 from flask import Flask, request, Response, render_template, jsonify
 
-#Get Master from Deezer
-def gather_deezer(song_s, artist_s):
-        master_info = {}
-        try:
-
-            deez_url = f'https://api.deezer.com/search?q=artist:\"{artist_s}\" track:\"{song_s}\"'
-            deezer_q = requests.get(deez_url)
-            track = str(deezer_q.json()['data'][0]['id'])
-
-            deezer_track = requests.get('https://api.deezer.com/track/' + track)
-            data = deezer_track.json()
-
-
-            master_info['Master'] = 'Master Info from Deezer'
-            master_info['Recording Title'] = data['title']
-            master_info['Artist'] = data['artist']['name']
-            master_info['Album Title'] = data['album']['title']
-            master_info['ISRC'] =  data['isrc']
-            master_info['Release Date'] = data['release_date']
-            master_info['Is Explicit?']= data['explicit_lyrics']
-            master_info['BPM'] = data['bpm']
-            master_info['Deezer Track ID'] = data['id']
-            master_info['Link to Album on Deezer'] = data['album']['link']
-
-            master_info['Album Art']= data['album']['cover_medium']
-            master_df = pd.DataFrame([master_info])
-            master_df.set_index('Master', inplace = True)
-
-
-            return master_df.T
-
-        except:
-            return 'Could not find on Deezer.'
-
-
-
-#Helper functions to clean import pub
-def clean_title(song):
-    song = song.replace('About', '').replace('Lyrics', '').replace('lyrics', '')
-    try:
-        song = song.split('–')[1]
-    except:
-        pass
-    song = song.strip()
-    return song
-
-def clean_pub(string):
-    try:
-        string = string.split('©')[1]
-        string = string.split('Lyrics')[0]
-        if ', Inc.' in string:
-            string = string.replace(', Inc.', '')
-        if '\\n' in string:
-            string = string.replace('\\n', '')
-    except:
-        pass
-    return string.strip()
-
-#Pub Info from Lyric Freak
-def gather_lyricfreak(search):
-    pub_info = {}
-
-    try:
-        base = 'https://www.lyricsfreak.com/search.php?q='
-        query = search.replace('-', '').replace(' ', '%20')
-        res = requests.get(base + query)
-        soup = BeautifulSoup(res.content, 'lxml')
-        song = soup.find('a', {'class': 'song'})
-        url = song['href']
-
-        res_2 = requests.get('https://www.lyricsfreak.com' + url)
-        soup_2 = BeautifulSoup(res_2.content, 'lxml')
-
-        title = soup_2.find('h2').text
-        artist = soup_2.find('a', {'class' : 'song-page-conthead-link'}).text
-
-
-        writer_pub = soup_2.find_all('div', {'class' : 'meta_l'})
-        writer = writer_pub[0].text
-        pub = writer_pub[1].text
-
-        if  soup_2.find('div', {'class' : 'lf-hero__subtitle'}):
-            album = soup_2.find('div', {'class' : 'lf-hero__subtitle'}).text
-            pub_info['Album'] = album.split('album:\n')[1].strip().split(' ')[0]
-
-        #Putting it into the dictionary
-        pub_info['Publishing'] = 'Publishing Info from Lyric Freak'
-        pub_info['Composition Title'] = clean_title(title)
-        pub_info['Performed By'] = artist
-        pub_info['Writers'] = writer.replace('Songwriters: ', '')
-        pub_info['Publishers'] = clean_pub(pub)
-
-        pub_df = pd.DataFrame([pub_info])
-
-        pub_df.set_index('Publishing', inplace = True)
-
-        return pub_df.T
-
-    except:
-        return f'Could not find on Lyric Freak.'
-
-#Genius Lyric Embed
-def gather_genius(search):
-    from credentials import genius
-    header = {'Authorization' : f'Bearer {genius}'}
-
-    res = requests.get('https://api.genius.com/search?q=' + search, headers = header)
-
-
-    song = res.json()['response']['hits'][0]['result']['api_path']
-    res2 = requests.get('https://api.genius.com' + song, headers = header)
-    embed = res2.json()['response']['song']['embed_content']
-
-    return embed
-
-from credentials import s_key, ss_key
-
-#Spotify auth
+#Get Spotify Master and Audio Features
+#First, Authorize
 def auth(key, sec):
-
+    from credentials import s_key, ss_key
     authorize = 'https://accounts.spotify.com/api/token'
     param = {
     "Content-Type": "application/x-www-form-urlencoded",
@@ -135,9 +20,11 @@ def auth(key, sec):
 
     return token
 
-#Spotify search
 def get_spotify(search):
+    from credentials import s_key, ss_key
+
     spotify_info = {}
+    audio = {}
 
     token = auth(s_key, ss_key)
 
@@ -162,73 +49,162 @@ def get_spotify(search):
 
         results = res.json()
 
-        spotify_info['Artist'] = results['tracks']['items'][0]['artists'][0]['name']
         spotify_info['Song Title'] = results['tracks']['items'][0]['name']
+        spotify_info['Artist'] = results['tracks']['items'][0]['artists'][0]['name']
+        spotify_info['Album'] = results['tracks']['items'][0]['album']['name']
+        spotify_info['ISRC'] = results['tracks']['items'][0]['external_ids']['isrc']
+        spotify_info['Release Date'] = results['tracks']['items'][0]['album']['release_date']
+        spotify_info['Is Explicit?'] = results['tracks']['items'][0]['explicit']
+        spotify_info['Track ID on Site'] = results['tracks']['items'][0]['id']
+        spotify_info['Master'] = 'Spotify'
+
+
+        isrc = results['tracks']['items'][0]['external_ids']['isrc']
         uri = results['tracks']['items'][0]['id']
         album_id = results['tracks']['items'][0]['album']['id']
         embed = f'<iframe src="https://open.spotify.com/embed/album/{album_id}" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>'
+        art = results['tracks']['items'][0]['album']['images'][1]['url']
 
         res_2 = requests.get(f'https://api.spotify.com/v1/audio-features/{uri}', headers = header)
 
         results_2 = res_2.json()
 
-        spotify_info['Spotify'] = 'Spotify Audio Features'
-        spotify_info['Danceeability'] = results_2['danceability']
-        spotify_info['Energy'] = results_2['energy']
-        spotify_info['Key'] = results_2['key']
-        spotify_info['Loudness'] = results_2['loudness']
-        spotify_info['Mode'] = results_2['mode']
-        spotify_info['Speechiness'] = results_2['speechiness']
-        spotify_info['Acousticness'] = results_2['acousticness']
-        spotify_info['Instrumentalness'] = results_2['instrumentalness']
-        spotify_info['Liveness'] = results_2['liveness']
-        spotify_info['Valence'] = results_2['valence']
-        spotify_info['Tempo'] = results_2['tempo']
-        spotify_info['Time Signature'] = results_2['time_signature']
+        audio['Spotify'] = 'Spotify Audio Features'
+        audio['Danceability'] = results_2['danceability']
+        audio['Energy'] = results_2['energy']
+        audio['Key'] = results_2['key']
+        audio['Loudness'] = results_2['loudness']
+        audio['Mode'] = results_2['mode']
+        audio['Speechiness'] = results_2['speechiness']
+        audio['Acousticness'] = results_2['acousticness']
+        audio['Instrumentalness'] = results_2['instrumentalness']
+        audio['Liveness'] = results_2['liveness']
+        audio['Valence'] = results_2['valence']
+        audio['Tempo'] = results_2['tempo']
+        audio['Time Signature'] = results_2['time_signature']
+
+        res_3 = requests.get(f'https://api.spotify.com/v1/albums/{album_id}', headers = header)
+
+
+        results_3 = res_3.json()
+        spotify_info['Album UPC'] = results_3['external_ids']['upc']
+        spotify_info['Label'] = results_3['label']
 
     except:
         return 'Could not find on Spotify.'
 
-    spotify_df = pd.DataFrame([spotify_info])
 
-    spotify_df.set_index('Spotify', inplace = True)
+    search_term = spotify_info['Song Title'] + ' ' + spotify_info['Artist']
 
-    return spotify_df.T, embed
+    return spotify_info, audio, embed, art, isrc, search_term
+
+#Next, get pub from MusicBrainz
+def get_musbrn(rec_isrc):
+    musicbrainzngs.set_useragent('sync_link', '1.0', contact='synclink.io')
+    result = musicbrainzngs.get_recordings_by_isrc(rec_isrc)
+
+    try:
+        mbid = result['isrc']['recording-list'][0]['id']
+
+        result_2 = musicbrainzngs.get_recording_by_id(mbid, includes=['work-level-rels', 'work-rels'])
+        title = result_2['recording']['title']
 
 
+        work_id = result_2['recording']['work-relation-list'][0]['work']['id']
+
+    except:
+        mbid = result['isrc']['recording-list'][1]['id']
+
+        result_2 = musicbrainzngs.get_recording_by_id(mbid, includes=['work-level-rels', 'work-rels'])
+        title = result_2['recording']['title']
+
+
+        work_id = result_2['recording']['work-relation-list'][0]['work']['id']
+
+
+    result_3 = musicbrainzngs.get_work_by_id(work_id,
+                                          includes=['annotation', 'aliases',
+                                                   'artist-rels', 'label-rels',  'work-rels'])
+
+    writer = []
+    for i in result_3['work']['artist-relation-list']:
+        if i['type'] == 'writer':
+            writer.append(i['artist']['name'])
+
+    n_writer = len(writer)
+
+    pub = []
+    for i in result_3['work']['label-relation-list']:
+        if i['type'] == 'publishing':
+            pub.append(i['label']['name'])
+
+    n_pub = len(pub)
+
+    result_4 = requests.get(f'https://musicbrainz.org/ws/2/work/{work_id}?fmt=json')
+    iswc = result_4.json()['iswcs']
+
+    if len(iswc) == 0:
+        iswc = "-"
+
+    pub = {
+        'Composition Title' : title,
+        'Writers' : ', '.join(writer),
+        'Publishers' : ', '.join(pub),
+        'ISWC' : ''.join(iswc),
+        'Publishing' : 'MusicBrainz'
+    }
+
+
+
+    return pub, n_pub, n_writer
+
+#Gather lyrics from lyrics
+def gather_genius(search):
+    from credentials import genius
+    header = {'Authorization' : f'Bearer {genius}'}
+
+    res = requests.get('https://api.genius.com/search?q=' + search, headers = header)
+
+
+    song = res.json()['response']['hits'][0]['result']['api_path']
+    res2 = requests.get('https://api.genius.com' + song, headers = header)
+    embed = res2.json()['response']['song']['embed_content']
+
+    return embed
+
+#Score the model
 features = ['year', 'explicit',
        'n_writers', 'n_pub',
        's_dance', 's_energy', 's_key', 's_loudness', 's_mode',
        's_speech', 's_acoustic', 's_inst', 's_live', 's_valence', 's_tempo',
        's_time_sig']
 
-#Sync Score
-def sync_score(mast, pub, spot):
+def sync_score(mast, pub, audio, publishers, writers):
     cvec = pickle.load(open('./model/cvec.pkl', 'rb'))
     rf = pickle.load(open('./model/rf.pkl', 'rb'))
 
 
-    pred_df = {}
-    pred_df['year'] = mast.iloc[4, 0][:4]
-    pred_df['explicit'] = int(mast.iloc[5, 0])
-    pred_df['n_writers'] = len(pub.iloc[2, 0].split(','))
-    pred_df['n_pub'] = len(pub.iloc[3, 0].split(','))
+    pred_df = {
+    'year' : mast['Release Date'][:4],
+    'explicit' : int(mast['Is Explicit?']),
+    'n_writers' : writers,
+    'n_pub' : publishers,
+    's_dance' : audio['Danceability'],
+    's_energy' : audio['Energy'],
+    's_key' : audio['Key'],
+    's_loudness' : audio['Loudness'],
+    's_mode' : audio['Mode'],
+    's_speech' : audio['Speechiness'],
+    's_acoustic' : audio['Acousticness'],
+    's_inst' : audio['Instrumentalness'],
+    's_live': audio['Liveness'],
+    's_valence' : audio['Valence'],
+    's_tempo': audio['Tempo'],
+    's_time_sig' : audio['Time Signature']
+        }
+    pred_df['text'] = mast['Artist'].lower() + " " + pub['Writers'] + " " + pub['Publishers']
 
 
-    pred_df['s_dance'] = spot.iloc[2, 0]
-    pred_df['s_energy'] = spot.iloc[3, 0]
-    pred_df['s_key']= spot.iloc[4, 0]
-    pred_df['s_loudness']= spot.iloc[5, 0]
-    pred_df['s_mode']= spot.iloc[6, 0]
-    pred_df['s_speech']= spot.iloc[7, 0]
-    pred_df['s_acoustic']= spot.iloc[8, 0]
-    pred_df['s_inst']= spot.iloc[9, 0]
-    pred_df['s_live']= spot.iloc[10, 0]
-    pred_df['s_valence'] = spot.iloc[11, 0]
-    pred_df['s_tempo'] = spot.iloc[12, 0]
-    pred_df['s_time_sig'] = spot.iloc[13, 0]
-
-    pred_df['text'] = mast.iloc[1, 0].lower() + " " + pub.iloc[2, 0] + " " + pub.iloc[3, 0]
 
     to_predict = pd.DataFrame([pred_df])
     vec = cvec.transform(to_predict['text'])
@@ -237,53 +213,156 @@ def sync_score(mast, pub, spot):
     X = pd.concat([x_text, to_predict[features]], axis = 1)
     score =  rf.predict_proba(X)[0][1]
 
-    if score >= .50:
+    if score >= .20:
         return 'Syncable'
 
     else:
         return 'Not very syncable'
 
+#More master info from Deezer
+def get_deezer(song_s, artist_s):
+
+
+    try:
+        deez_url = f'https://api.deezer.com/search?q=artist:\"{artist_s}\" track:\"{song_s}\"'
+        deezer_q = requests.get(deez_url)
+        track = str(deezer_q.json()['data'][0]['id'])
+
+        deezer_track = requests.get('https://api.deezer.com/track/' + track)
+        data = deezer_track.json()
+
+        al = str(data['album']['id'])
+        deezer_album = requests.get('https://api.deezer.com/album/' + al)
+        album = deezer_album.json()
+
+        master_info = {'Song Title': data['title'],
+                         'Artist': data['artist']['name'],
+                         'Album': data['album']['title'],
+                         'ISRC': data['isrc'],
+                         'Release Date': data['release_date'],
+                         'Is Explicit?': data['explicit_lyrics'],
+                         'Track ID on Site': data['id'],
+                         'Master': 'Deezer',
+                         'Album UPC': album['upc'],
+                         'Label': album['label']}
+    except:
+
+        master_info = {'Song Title': '-',
+                     'Artist': '-',
+                     'Album': '-',
+                     'ISRC': '-',
+                     'Release Date': '-',
+                     'Is Explicit?': '-',
+                     'Track ID on Site': '-',
+                     'Master': 'Not Found on Deezer',
+                     'Album UPC': '-',
+                     'Label': '-'}
+
+
+
+
+    return master_info
+
+#More pub from LyricsFreak
+def clean_title(song):
+    song = song.replace('About', '').replace('Lyrics', '').replace('lyrics', '')
+    try:
+        song = song.split('–')[1]
+    except:
+        pass
+    song = song.strip()
+    return song
+
+def clean_pub(string):
+    try:
+        string = string.split('©')[1]
+        string = string.split('Lyrics')[0]
+        if ', Inc.' in string:
+            string = string.replace(', Inc.', '')
+        if '\\n' in string:
+            string = string.replace('\\n', '')
+    except:
+        pass
+    return string.strip()
+
+def gather_lyricfreak(search):
+
+    try:
+        base = 'https://www.lyricsfreak.com/search.php?q='
+        query = search.replace('-', '').replace(' ', '%20')
+        res = requests.get(base + query)
+        soup = BeautifulSoup(res.content, 'lxml')
+        song = soup.find('a', {'class': 'song'})
+        url = song['href']
+
+        res_2 = requests.get('https://www.lyricsfreak.com' + url)
+        soup_2 = BeautifulSoup(res_2.content, 'lxml')
+
+        title = soup_2.find('h2').text
+        artist = soup_2.find('a', {'class' : 'song-page-conthead-link'}).text
+
+
+        writer_pub = soup_2.find_all('div', {'class' : 'meta_l'})
+        writer = writer_pub[0].text
+        pub = writer_pub[1].text
+
+        #Putting it into the dictionary
+        pub_dict = {'Composition Title': clean_title(title),
+             'Writers': writer.replace('Songwriters: ', ''),
+             'Publishers': clean_pub(pub),
+             'ISWC': '-',
+             'Publishing': 'LyricsFreak'}
+
+    except:
+        pub_dict = {'Composition Title': '-',
+             'Writers': '-',
+             'Publishers': '-',
+             'ISWC': '-',
+             'Publishing': 'LyricsFreak'}
+
+    return pub_dict
+
+#Helper to combine the master/pub info
+def to_df(spot, deez, mb, lf):
+    master = pd.DataFrame([spot, deez]).set_index('Master')
+    publishing = pd.DataFrame([mb, lf]).set_index('Publishing')
+
+    #Clean up the explicit field to convent boolean
+    exp_dict = {False : 'No', True : 'Yes'}
+
+    master['Is Explicit?'] = master['Is Explicit?'].map(exp_dict)
+
+    return master.T, publishing.T
+
 #Put it all together
 def get_all(artist, title):
-    master = gather_deezer(title, artist)
+    search = artist.lower() + ' ' + title.lower()
 
-    if type(master) != str:
-        title = master.iloc[0, 0].lower()
-        artist = master.iloc[1, 0].lower()
-        title_display = master.iloc[0, 0]
-        artist_display = master.iloc[1, 0]
-        search_new = title + ' ' + artist
-        artwork = master.iloc[9, 0]
+    #Check Spotify and get ISRC
+    spot, audio, embed, art, isrc, search_new = get_spotify(search)
 
-        genius_lyrics = gather_genius(search_new)
+    #Also check Deezer
+    deez = get_deezer(title, artist)
 
-        publishing = gather_lyricfreak(search_new)
+    #Get Pub info from Music Brainz
+    mb, publishers, writers = get_musbrn(isrc)
 
-        spotify, embed = get_spotify(search_new)
+    #And Pub from LyricsFreak
+    lf = gather_lyricfreak(search_new)
 
-        if (type(publishing != str)) and (type(spotify != str)):
-            score = sync_score(master, publishing, spotify)
-        else:
-            score = 'Not enough info for Sync Score'
+    #Lyrics from Genius
+    lyrics = gather_genius(search_new)
 
-    else:
-        title_display = "None"
-        artist_display = "None"
-        master =  "None"
-        publishing = "None"
-        genius_lyrics = "None"
-        spotify = "None"
-        score = "None"
-        embed = "None"
-        artwork = 'None'
+    #Get the Score
+    score = sync_score(spot, mb, audio, publishers, writers)
 
-    if master.iloc[5, 0] == False:
-        master.iloc[5, 0] = 'No'
-    else:
-        master.iloc[5, 0] = 'Yes'
+    #Title/Artist to display
+    title_display = spot['Song Title']
+    artist_display = spot['Artist']
 
-    return artwork, title_display, artist_display, master, publishing, genius_lyrics, spotify, score, embed
+    master, publishing = to_df(spot, deez, mb, lf)
 
+    return art, title_display, artist_display, master, publishing, lyrics, audio, score, embed
 
 app = Flask('myApp')
 
@@ -295,13 +374,24 @@ def form():
 @app.route('/submit')
 
 def submit():
-    user_input = request.args
-
-    art, title_display, artist_display, master, publishing, genius_lyrics, spotify, score, embed = get_all(user_input['artist'], user_input['title'])
     try:
-        return render_template('results.html', master = master.iloc[0:8, :].to_html(), pub = publishing.to_html(), spot = spotify.to_html(), title_display = title_display, artist_display = artist_display, artwork=art, lyrics=genius_lyrics, embed = embed, score=score)
+        user_input = request.args
+
+        art, title_display, artist_display, master, publishing, genius_lyrics, spotify, score, embed = get_all(user_input['artist'], user_input['title'])
+
+        return render_template('results.html', master = master.iloc[0:10, :].to_html(), pub = publishing.to_html(), title_display = title_display, artist_display = artist_display, artwork=art, lyrics=genius_lyrics, embed = embed, score=score)
     except:
-        return "Could not find"
+        return render_template('no_results.html')
+
+@app.route('/no_results')
+
+def no_results():
+    return render_template('no_results.html')
+
+@app.route('/resources')
+
+def resources():
+    return render_template('resources.html')
 
 if __name__ == '__main__':
     app.run(debug = True)
